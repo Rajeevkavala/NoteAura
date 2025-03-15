cc3e81e86d7c761f62f9277f731f38146593bde5
Rajeevkavala <68290177+Rajeevkavala@users.noreply.github.com>
2025-03-15 14:15:30 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
 "use client";
 import "./globals.css";
-import { Poppins } from "next/font/google"; 
 import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import ProgressBar from "@/components/ui/ProgressBar"; // Import Progress Bar
 
-
-const poppins = Poppins({ subsets: ["latin"], weight: "400", display: "swap" });
 
 export default function RootLayout({ children }) {
   return (
